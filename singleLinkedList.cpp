8be23949138e63714f1bd4fbceeeb107d530b2af
riswandaP <riswandaputra03@gmail.com>
2025-05-08 20:21:49 +0700

    memuat case 4,5, dan while

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -161,3 +161,39 @@ int main()
             mhs.traverse();
         }
         break;
+
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";    
+                break;
+            }
+            Node *previous, *current;
+            cout    << endl
+                    << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: '" << current->noMhs << "'" << endl;
+                cout << "\n";
+            }
+        }
+        break;
+        case '5':
+        {
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !" << endl;
+        }
+        break;
+    }
+    } while (ch != '5');
+}
