7abbbde7d679ab372b80cc8d1b8538388302355a
riswandaP <riswandaputra03@gmail.com>
2025-05-08 20:20:36 +0700

    membuat do

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -117,3 +117,22 @@ int main()
     LinkedList mhs;
     int nim;
     char ch;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam 'list'" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+            break;
+        }
